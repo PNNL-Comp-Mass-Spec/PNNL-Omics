e4022c41b91c2671b15f64320357caf5fd8894b5
Kevin Crowell <Kevin.Crowell@pnnl.gov>
2010-06-08 19:57:22 +0000

    - Added AnonymousComparer and a Code Example to go with it

---
+++ PNNLOmics/Algorithms/FeatureFinding/CodeExamples/BinarySearchExample.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PNNLOmics.Data.Features;
+using PNNLOmics.Generic;
+
+namespace PNNLOmics.Algorithms.FeatureFinding.CodeExamples
+{
+	public class BinarySearchExample
+	{
+		public static void Example()
+		{
+			AnonymousComparer<LCMSFeature> comparer = new AnonymousComparer<LCMSFeature>(new Comparison<LCMSFeature>(UMC.MassMaxComparison));
+		}
+
+		private void SearchAndInsert(List<LCMSFeature> lcmsFeatureList, LCMSFeature lcmsFeature, AnonymousComparer<LCMSFeature> comparer)
+		{
+			int index = lcmsFeatureList.BinarySearch(lcmsFeature, comparer);
+			lcmsFeatureList.Insert(Math.Abs(index), lcmsFeature);
+		}
+	}
+}
---
+++ PNNLOmics/Generic/AnonymousComparer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PNNLOmics.Generic
+{
+	public class AnonymousComparer<T> : IComparer<T>
+	{
+		private Comparison<T> comparison;
+
+		public AnonymousComparer(Comparison<T> comparison)
+		{
+			if (comparison == null)
+			{
+				throw new ArgumentNullException("comparison");
+			}
+			this.comparison = comparison;
+		}
+
+		public int Compare(T x, T y)
+		{
+			return comparison(x, y);
+		}
+	}
+}
