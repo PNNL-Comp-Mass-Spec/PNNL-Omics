528091c076d2286d9b582327d063377e09e18491
Gordon Slysz <gordon.slysz@pnnl.gov>
2010-07-01 00:31:58 +0000

    - Added UnitTest project - added structure for testing STAC

---
+++ PNNLOmics.UnitTests/AlgorithmTests/STACTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using PNNLOmics.Data.Features;
+using PNNLOmics.Data;
+using PNNLOmics.Algorithms.FeatureMatcher.Data;
+
+namespace PNNLOmics.UnitTests.AlgorithmTests
+{
+    [TestFixture]
+    public class STACTests
+    {
+        [Test]
+        public void orbitrapDataStandardTest1()
+        {
+            List<UMC> umcList = new List<UMC>();
+            List<MassTag> massTagList = new List<MassTag>();
+
+            List<FeatureMatch<UMC, MassTag>> featureMatchList = new List<FeatureMatch<UMC, MassTag>>();
+          
+            loadOrbitrapData(umcList,massTagList);
+
+            bool useDriftDimension = false;
+            STACInformation stac = new STACInformation(useDriftDimension);
+
+            //stac.PerformSTAC<UMC,MassTag>(
+        }
+
+        
+        
+        
+        private void loadOrbitrapData(List<UMC> umcList, List<MassTag> massTagList)
+        {
+
+
+            //TODO: Gord - add code for importing UMC files. 
+            //TODO: Gord - add code for importing MassTag data from a .txt file. 
+
+            string umcFilePath = FileReferences.OrbitrapUMCFile1;
+            string massTagFilePath = FileReferences.OrbitrapMassTagFile;
+
+
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
---
+++ PNNLOmics.UnitTests/FileReferences.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace PNNLOmics.UnitTests
+{
+    /// <summary>
+    /// Contains references to standard files shared by many tests
+    /// </summary>
+    public class FileReferences
+    {
+
+        public static string OrbitrapStandardRAWFile = "";
+
+        public static string UIMFFPGAStandardFile = "";
+
+        public static string UIMFAcquirisStandardFile = "";
+
+        public static string Bruker9TStandardFile = "";
+
+        public static string Bruker12TStandardFile = "";
+
+
+        public static string OrbitrapUMCFile1 = "..\\..\\TestFiles\\STAC_related\\QC_Shew_08_04-pt5-2_11Jan09_Sphinx_08-11-18_UMCs.txt";
+        public static string OrbitrapMassTagFile = "..\\..\\TestFiles\\STAC_related\\QC_Shew_MTs.txt";
+
+
+    }
+}
