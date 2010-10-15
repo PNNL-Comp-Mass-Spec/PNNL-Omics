99215451c7ee824d32852bdb74e3831528f80692
Scott Kronewitter <scott.kronewitter@pnnl.gov>
2010-10-15 00:43:45 +0000

    Constants:  Updated Constants class and class diagram

--- PNNLOmics/Data/Constants/Constants.cs
+++ PNNLOmics/Data/Constants/Constants.cs
@@ -4,15 +4,15 @@ using PNNLOmics.Data.Constants.Utilities;
 
 namespace PNNLOmics.Data.Constants
 {
-	/// <summary>
-	/// This class contains the constants libraries
-	/// </summary>
+    /// <summary>
+    /// This class contains the constants libraries
+    /// </summary>
     public static class Constants
     {
-		/// <summary>
-		/// Amino acid library instance.
-		/// </summary>
-		private static AminoAcidLibrary m_aminoAcidLibrary;
+        /// <summary>
+        /// Amino acid library instance.
+        /// </summary>
+        private static AminoAcidLibrary m_aminoAcidLibrary;
         /// <summary>
         /// Cross ring library instance.
         /// </summary>
@@ -20,7 +20,7 @@ namespace PNNLOmics.Data.Constants
         /// <summary>
         /// Element library instance.
         /// </summary>
-		private static ElementLibrary   m_elementLibrary;
+        private static ElementLibrary m_elementLibrary;
         /// <summary>
         /// Miscellaneous matter library instance.
         /// </summary>
@@ -35,24 +35,24 @@ namespace PNNLOmics.Data.Constants
         private static SubAtomicParticleLibrary m_subAtomicParticleLibrary;
 
 
-		#region Library Properties
-		/// <summary>
-		/// Gets the amino acid library.
-		/// </summary>
-		public static AminoAcidLibrary AminoAcids
-		{
-			get
-			{
-				if (m_aminoAcidLibrary == null)
-				{
-					m_aminoAcidLibrary = new AminoAcidLibrary();
-					m_aminoAcidLibrary.LoadLibrary();
-				}
-				return m_aminoAcidLibrary;
-			}
-		}
+        #region Library Properties
+        /// <summary>
+        /// Gets the amino acid library.
+        /// </summary>
+        public static AminoAcidLibrary AminoAcids
+        {
+            get
+            {
+                if (m_aminoAcidLibrary == null)
+                {
+                    m_aminoAcidLibrary = new AminoAcidLibrary();
+                    m_aminoAcidLibrary.LoadLibrary();
+                }
+                return m_aminoAcidLibrary;
+            }
+        }
 
-         /// <summary>
+        /// <summary>
         /// Gets the monosaccharide cross ring cleavage library.
         /// </summary>
         public static CrossRingLibrary CrossRings
@@ -68,21 +68,21 @@ namespace PNNLOmics.Data.Constants
             }
         }
 
-		/// <summary>
-		/// Gets the amino acid library.
-		/// </summary>
-		public static ElementLibrary Elements
-		{
-			get
-			{
-				if (m_elementLibrary == null)
-				{
-					m_elementLibrary = new ElementLibrary();
-					m_elementLibrary.LoadLibrary();
-				}
-				return m_elementLibrary;
-			}
-		}
+        /// <summary>
+        /// Gets the amino acid library.
+        /// </summary>
+        public static ElementLibrary Elements
+        {
+            get
+            {
+                if (m_elementLibrary == null)
+                {
+                    m_elementLibrary = new ElementLibrary();
+                    m_elementLibrary.LoadLibrary();
+                }
+                return m_elementLibrary;
+            }
+        }
 
         /// <summary>
         /// Gets the miscellaneous matter library.
@@ -131,184 +131,6 @@ namespace PNNLOmics.Data.Constants
                 return m_subAtomicParticleLibrary;
             }
         }
-		#endregion
-
-        #region properties
-
-        //private static LibraryCompound m_MonosaccharideLibrary { get; set; }
-        //private static LibraryCompound m_CrossRingLibrary { get; set; }
-        //private static LibraryCompound m_MiscellaneousMatterLibrary { get; set; }
-        //private static LibrarySubAtomicParticle m_SubAtomicParticleLibrary { get; set; }
-        //private static LibraryElement m_ElementLibrary { get; set; }
-
-		
-
-        //private static LibraryCompound LibraryMonosaccharide //m_MonosaccharideLibrary
-        //{
-        //    get
-        //    {
-        //        if (m_MonosaccharideLibrary == null)
-        //        {
-        //            Dictionary<string, Compound> compoundDictionary = MonosaccharideLibrary.LoadLibrary();
-
-        //            int count = 0;
-        //            Dictionary<int, string> enumDictionary = new Dictionary<int, string>();
-        //            foreach (KeyValuePair<string, Compound> item in compoundDictionary)
-        //            {
-        //                enumDictionary.Add(count, item.Key);
-        //                count++;
-        //            }
-
-        //            LibraryCompound compoundLibrary = new LibraryCompound();
-        //            compoundLibrary.ConstantsDictionary = compoundDictionary;
-        //            compoundLibrary.ConstantsEnumDictionary = enumDictionary;
-        //            m_MonosaccharideLibrary = compoundLibrary;
-        //        }
-        //        return m_MonosaccharideLibrary;
-        //    }
-        //}
-
-        //private static LibraryCompound LibraryCrossRing //m_CrossRingLibrary
-        //{
-        //    get
-        //    {
-        //        if (m_CrossRingLibrary == null)
-        //        {
-        //            Dictionary<string, Compound> compoundDictionary = CrossRingLibrary.LoadCrossRingData();
-
-        //            int count = 0;
-        //            Dictionary<int, string> enumDictionary = new Dictionary<int, string>();
-        //            foreach (KeyValuePair<string, Compound> item in compoundDictionary)
-        //            {
-        //                enumDictionary.Add(count, item.Key);
-        //                count++;
-        //            }
-
-        //            LibraryCompound compoundLibrary = new LibraryCompound();
-        //            compoundLibrary.ConstantsDictionary = compoundDictionary;
-        //            compoundLibrary.ConstantsEnumDictionary = enumDictionary;
-        //            m_CrossRingLibrary = compoundLibrary;
-        //        }
-        //        return m_CrossRingLibrary;
-        //    }
-        //}
-
-        //private static LibraryCompound LibraryMiscellaneousMatter //m_MiscellaneousMatterLibrary
-        //{
-        //    get
-        //    {
-        //        if (m_MiscellaneousMatterLibrary == null)
-        //        {
-        //            Dictionary<string, Compound> compoundDictionary = MiscellaneousMatterLibrary.LoadLibrary();
-
-        //            int count = 0;
-        //            Dictionary<int, string> enumDictionary = new Dictionary<int, string>();
-        //            foreach (KeyValuePair<string, Compound> item in compoundDictionary)
-        //            {
-        //                enumDictionary.Add(count, item.Key);
-        //                count++;
-        //            }
-
-        //            LibraryCompound compoundLibrary = new LibraryCompound();
-        //            compoundLibrary.ConstantsDictionary = compoundDictionary;
-        //            compoundLibrary.ConstantsEnumDictionary = enumDictionary;
-        //            m_MiscellaneousMatterLibrary = compoundLibrary;
-        //        }
-        //        return m_MiscellaneousMatterLibrary;
-        //    }
-        //}
-
-        //private static LibrarySubAtomicParticle LibrarySubAtomicParticle //m_SubAtomicParticleLibrary
-        //{
-        //    get
-        //    {
-        //        if (m_SubAtomicParticleLibrary == null)
-        //        {
-        //            Dictionary<string, SubAtomicParticle> subAtomicParticleDictionary = SubAtomicParticleLibrary.LoadSubAtomicParticleData();
-
-        //            int count = 0;
-        //            Dictionary<int, string> enumDictionary = new Dictionary<int, string>();
-        //            foreach (KeyValuePair<string, SubAtomicParticle> item in subAtomicParticleDictionary)
-        //            {
-        //                enumDictionary.Add(count, item.Key);
-        //                count++;
-        //            }
-
-        //            LibrarySubAtomicParticle subAtomicParticleLibrary = new LibrarySubAtomicParticle();
-        //            subAtomicParticleLibrary.ConstantsDictionary = subAtomicParticleDictionary;
-        //            subAtomicParticleLibrary.ConstantsEnumDictionary = enumDictionary;
-        //            m_SubAtomicParticleLibrary = subAtomicParticleLibrary;
-        //        }
-        //        return m_SubAtomicParticleLibrary;
-        //    }
-        //}
-
-        //private static LibraryElement LibraryElement //m_ElementLibrary   
-        //{
-        //    get
-        //    {
-        //        if (m_ElementLibrary == null)
-        //        {
-        //            Dictionary<string, Element> elementDictionary = ElementLibrary.LoadLibrary();
-
-        //            int count = 0;
-        //            Dictionary<int, string> enumDictionary = new Dictionary<int, string>();
-        //            foreach (KeyValuePair<string, Element> item in elementDictionary)
-        //            {
-        //                enumDictionary.Add(count, item.Key);
-        //                count++;
-        //            }
-
-        //            LibraryElement elementLibrary = new LibraryElement();
-        //            elementLibrary.ConstantsDictionary = elementDictionary;
-        //            elementLibrary.ConstantsEnumDictionary = enumDictionary;
-        //            m_ElementLibrary = elementLibrary;
-        //        }
-        //        return m_ElementLibrary;
-        //    }
-        //}
-
         #endregion
-
-       
-        ///// <summary>
-        ///// 
-        ///// </summary>
-        ///// <param name="chosenKey"></param>
-        ///// <param name="isotopeKey"></param>
-        ///// <returns></returns>
-        //public static double GetIsotopeMass(ChoicesElements elementKey, string isotopeKey)
-        //{
-        //    Elements.GetMonoisotopicMass(PNNLOmics.Data.Constants.ChoicesElements.Actinium);
-        //    double data =  Elements[PNNLOmics.Data.Constants.ChoicesElements.Actinium].MassAverage;
-			
-        //    Element element = Elements.GetElement(elementKey);
-        //    return element.IsotopeDictionary[isotopeKey].Mass;
-        //}
-
-        //public static double GetIsotopeAbundance(ChoicesElements elementKey, string isotopeKey)
-        //{
-        //    Element element = Elements.GetElement(elementKey);
-        //    return element.IsotopeDictionary[isotopeKey].Mass;            
-        //}
-
-        //public static int GetIsotopeNumber(ChoicesElements elementKey, string isotopeKey)
-        //{
-        //    Element element = Elements.GetElement(elementKey);
-        //    return element.IsotopeDictionary[isotopeKey].Mass;
-        //}        		
     }
 }
-
-/* 
-			 * how to get the list of all masses for amino acids.
-			// Array of constants.
-			Array values		= Enum.GetValues(typeof(AminoAcid));
-			AminoAcid[] acids	= new AminoAcid[values.Length];
-			values.CopyTo(acids, 0);
-
-			foreach (AminoAcid acid in acids)
-			{
-				AminoAcids.GetMonoisotopicMass(acid);
-			}
-		*/			         
