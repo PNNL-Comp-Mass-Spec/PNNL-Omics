53de144a760b5c024b8eacb14e4e8d1ef8195316
Gordon Slysz <gordon.slysz@pnnl.gov>
2010-12-15 20:59:30 +0000

    added PeakDetectorTests

---
+++ PNNLOmics.UnitTests/AlgorithmTests/PeakDetectorTests/PeakDetectorTests.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using NUnit.Framework;
+using PNNLOmics.Algorithms.PeakDetector;
+using PNNLOmics.Data;
+using System.IO;
+
+namespace PNNLOmics.UnitTests.AlgorithmTests.PeakDetectorTests
+{
+    [TestFixture]
+    public class PeakDetectorTests
+    {
+        [Test]
+        public void PeakDetectorV3_DiscoverPeaks_no_ThresholdingTest1()
+        {
+
+            double[] xvals = null;
+            double[] yvals = null;
+
+            int scanNum = 0;
+
+            loadTestScanData(ref xvals, ref yvals);
+            Assert.That(xvals != null);
+            Assert.AreEqual(322040, xvals.Length);
+
+            List<PNNLOmics.Data.XYData> testXYData = convertXYDataToOMICSXYData(xvals, yvals);
+
+            PeakCentroidParameters parametersPeakCentroid = new PeakCentroidParameters();
+            parametersPeakCentroid.ScanNumber = scanNum;
+
+            List<CentroidedPeak> centroidedPeakList = new List<CentroidedPeak>();
+            centroidedPeakList = PeakCentroid.DiscoverPeaks(testXYData, parametersPeakCentroid);
+
+            Assert.That(centroidedPeakList.Count > 0);
+
+            displayPeakData(centroidedPeakList);
+        }
+
+
+        [Test]
+        public void PeakDetectorV3_DiscoverPeaksThenThreshold()
+        {
+            double[] xvals = null;
+            double[] yvals = null;
+
+            int scanNum = 0;
+
+            loadTestScanData(ref xvals, ref yvals);
+            Assert.That(xvals != null);
+            Assert.AreEqual(322040, xvals.Length);
+
+            List<PNNLOmics.Data.XYData> testXYData = convertXYDataToOMICSXYData(xvals, yvals);
+
+            PeakCentroidParameters parametersPeakCentroid = new PeakCentroidParameters();
+            parametersPeakCentroid.ScanNumber = scanNum;
+
+            List<CentroidedPeak> centroidedPeakList = new List<CentroidedPeak>();
+            centroidedPeakList = PeakCentroid.DiscoverPeaks(testXYData, parametersPeakCentroid);
+
+            PeakThresholdParameters parametersThreshold = new PeakThresholdParameters();
+            parametersThreshold.SignalToShoulderCuttoff = 3f;
+
+            List<CentroidedPeak> thresholdedData = new List<CentroidedPeak>();
+            thresholdedData = PeakThreshold.ApplyThreshold(ref centroidedPeakList, parametersThreshold);
+
+            Assert.That(centroidedPeakList.Count > 0);
+
+            displayPeakData(thresholdedData);
+            Console.WriteLine();
+            Console.WriteLine("Non-thresholded Candidate Peaks detected = " + centroidedPeakList.Count);
+            Console.WriteLine("Thresholded Peaks detected = " + thresholdedData.Count);
+
+
+        }
+
+
+        [Test]
+        public void PeakDetectorV1_DiscoverPeaks_Test1()
+        {
+            double[] xvals = null;
+            double[] yvals = null;
+
+            int scanNum = 0;
+
+            loadTestScanData(ref xvals, ref yvals);
+            Assert.That(xvals != null);
+            Assert.AreEqual(322040, xvals.Length);
+
+            double peakBR = 2;
+            double sigNoiseThresh = 3;
+
+            DeconTools.Backend.XYData xydata = new DeconTools.Backend.XYData();
+            xydata.Xvalues = xvals;
+            xydata.Yvalues = yvals;
+
+            DeconTools.Backend.ProcessingTasks.DeconToolsPeakDetector peakdetector = new DeconTools.Backend.ProcessingTasks.DeconToolsPeakDetector(peakBR, sigNoiseThresh, DeconTools.Backend.Globals.PeakFitType.QUADRATIC, false);
+            List<DeconTools.Backend.Core.IPeak> peakList = peakdetector.FindPeaks(xydata, 0, 50000);
+
+            displayPeakData(peakList);
+
+
+        }
+
+        private void displayPeakData(List<DeconTools.Backend.Core.IPeak> peakList)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var item in peakList)
+            {
+                sb.Append(item.XValue);
+                sb.Append('\t');
+                sb.Append(item.Height);
+                sb.Append('\t');
+
+                sb.Append(item.Width);
+                sb.Append(Environment.NewLine);
+
+            }
+            Console.WriteLine(sb.ToString());
+        }
+
+
+
+
+        private void displayPeakData(List<CentroidedPeak> centroidedPeakList)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var item in centroidedPeakList)
+            {
+                sb.Append(item.XValue);
+                sb.Append('\t');
+                sb.Append(item.Intensity);
+                sb.Append('\t');
+                sb.Append(item.Height);
+                sb.Append('\t');
+
+                sb.Append(item.Width);
+                sb.Append(Environment.NewLine);
+
+            }
+            Console.WriteLine(sb.ToString());
+        }
+
+        private List<XYData> convertXYDataToOMICSXYData(double[] xvals, double[] yvals)
+        {
+            List<XYData> xydataList = new List<XYData>();
+            for (int i = 0; i < xvals.Length; i++)
+            {
+                XYData xydatapoint = new XYData(xvals[i], yvals[i]);
+                xydataList.Add(xydatapoint);
+
+            }
+
+            return xydataList;
+
+        }
+
+        private void loadTestScanData(ref double[] xvals, ref double[] yvals)
+        {
+            string testScanFilePath = @"\\protoapps\UserData\Slysz\DeconTools_TestFiles\SyntheticMS\SyntheticMSScan00.txt";
+
+            DeconTools.Backend.Runs.RunFactory runFactory = new DeconTools.Backend.Runs.RunFactory();
+            DeconTools.Backend.Core.Run run = runFactory.CreateRun(testScanFilePath);
+
+            DeconTools.Backend.Core.ScanSet scan = new DeconTools.Backend.Core.ScanSet(0);
+            run.GetMassSpectrum(scan);
+
+            //run.XYData.Display();
+
+            xvals = run.XYData.Xvalues;
+            yvals = run.XYData.Yvalues;
+
+        }
+
+
+    }
+}
