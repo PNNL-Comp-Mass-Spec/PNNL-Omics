5c5e6552b10bc2e35bee6ed71b3c4dc91c1f7c8c
Scott Kronewitter <scott.kronewitter@pnnl.gov>
2011-01-07 19:40:39 +0000

    Peak Detection:  Cleaned up PeakThresholding parameters and removed ThresholdpeakObject.  Added enumerations for different data types so different thresholds can be used. All builds and tests work on local computer

---
+++ PNNLOmics/Algorithms/PeakDetection/PeakThresholdEnumeration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PNNLOmics.Algorithms.PeakDetection
+{
+    /// <summary>
+    /// Has the noise been removed yet.  Orbitrap data has the noise removed.
+    /// </summary>
+    public enum InstrumentDataNoiseType
+    {
+        Standard,
+        NoiseRemoved
+    }
+}
--- PNNLOmics/Algorithms/PeakDetection/PeakThresholdParameters.cs
+++ PNNLOmics/Algorithms/PeakDetection/PeakThresholdParameters.cs
@@ -8,47 +8,38 @@ namespace PNNLOmics.Algorithms.PeakDetection
 {
     public class PeakThresholdParameters
     {
+        /// <summary>
+        /// Have we applied a thresholding algorithm to the data
+        /// </summary>
         public bool isDataThresholded { get; set; }
-        public List<XYData> ThresholdedPeakData { get; set; }//results Npoints Long
-        public List<double> ThresholdedPeakFWHM { get; set; }//results NPoints Long
-        public List<double> ThresholdedPeakSNShoulder { get; set; }//resutls NpointsLong
-        public List<double> ThresholdedPeakSN { get; set; }//resutls NpointsLong
-        public List<double> ThresholdedPeakSignalToBackground { get; set; }//results NpointsLong
-        public List<ThresholdpeakObject> ThresholdedObjectlist { get; set; }//NpointsLong
+        
+        /// <summary>
+        /// Ratio of highest piont at the peak apex to the lowet peak valley.
+        /// </summary>
         public float SignalToShoulderCuttoff { get; set; }
+        
+        /// <summary>
+        /// How a threshold line is chosen.
+        /// </summary>
         public string ThresholdMethod { get; set; }
+        
+        /// <summary>
+        /// Scan number the data came from.
+        /// </summary>
         public int ScanNumber { get; set; }
+        
+        /// <summary>
+        /// Has the noise been removed yet.  Orbitrap data has the noise removed.
+        /// </summary>
+        public InstrumentDataNoiseType thresholdType { get; set; }
 
         public PeakThresholdParameters()
         {
             this.isDataThresholded = false;
-            this.ThresholdedPeakData = new List<XYData>();
-            this.ThresholdedPeakFWHM = new List<double>();
-            this.ThresholdedPeakSNShoulder = new List<double>();
-            this.ThresholdedPeakSN = new List<double>();
-            this.ThresholdedPeakSignalToBackground = new List<double>();
             this.SignalToShoulderCuttoff = 3;
-            this.ThresholdedObjectlist = new List<ThresholdpeakObject>();
             this.ThresholdMethod = "AveragePlusSigma";
             this.ScanNumber = 0;
-        }
-    }
-
-    public class ThresholdpeakObject
-    {
-        public XYData PeakData { get; set; }
-        public double PeakFWHM { get; set; }
-        public double PeakSNShoulder { get; set; }
-        public double PeakSignalToBackground { get; set; }
-        public double PeakSN { get; set; }
-
-        public ThresholdpeakObject()
-        {
-            this.PeakData = new XYData(0, 0);
-            this.PeakFWHM = 0;
-            this.PeakSNShoulder = 0;
-            this.PeakSignalToBackground = 0;
-            this.PeakSN = 0;
+            this.thresholdType = InstrumentDataNoiseType.Standard;
         }
     }
 }
