742ea0d4b5fedaf5323e8bcc3994b11bd7e8f5a6
Scott Kronewitter <scott.kronewitter@pnnl.gov>
2012-02-27 17:49:51 +0000

    Added PrecursorInfo.cs so that we have a data object to hold information pertaining to the precursor chosen for fragmentation

---
+++ PNNLOmics/Data/PrecursorInfo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PNNLOmics.Data
+{
+    public class PrecursorInfo
+    { 
+        /// <summary>
+        /// Peak coresponding to the Precursor selected for fragmentation
+        /// </summary>
+        public PNNLOmics.Data.XYData PrecursorXYData { get; set; }
+
+        /// <summary>
+        /// MS=0 or MSMS=1
+        /// </summary>
+        public int MSLevel { get; set; }
+
+        /// <summary>
+        /// Charge of the precursor Ion
+        /// </summary>
+        public int PrecursorCharge { get; set; }
+
+        /// <summary>
+        /// PrecursorScanNumber
+        /// </summary>
+        public int PrecursorScan { get; set; }
+
+        public PrecursorInfo()
+        {
+            PrecursorXYData = new PNNLOmics.Data.XYData(-1, -1);
+            MSLevel = -1;
+            PrecursorCharge = -1;
+            PrecursorScan = -1;
+        }
+    }
+}
