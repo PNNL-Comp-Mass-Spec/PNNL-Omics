822cf9cef7d01d9b1eb11c8c7154a318fe4a87c8
Scott Kronewitter <scott.kronewitter@pnnl.gov>
2012-07-10 02:24:33 +0000

    Added a constructor to the UserUnit so it is easier to create one with all the values

--- PNNLOmics/Data/Constants/UserUnit.cs
+++ PNNLOmics/Data/Constants/UserUnit.cs
@@ -12,14 +12,29 @@ namespace PNNLOmics.Data.Constants
     public class UserUnit : Matter
     {
         /// <summary>
-        /// constructor for a simple userUnit that has the required name and symbol
+        /// constructor for a minimal userUnit that has the required name and symbol
         /// </summary>
-        /// <param name="name"></param>
-        /// <param name="symbol"></param>
+        /// <param name="name">name to call the user unit such as sodium</param>
+        /// <param name="symbol">short name to reference it by, such as Na for sodium</param>
         public UserUnit(string name, string symbol)
         {
             this.Name = name;
             this.Symbol = symbol;
+        }
+
+        /// <summary>
+        /// constructor for a full userUnit that has the all fields
+        /// </summary>
+        /// <param name="name">name to call the user unit such as sodium</param>
+        /// <param name="symbol">short name to reference it by, such as Na for sodium</param>
+        /// <param name="mass">monoisotopic mass</param>
+        /// <param name="userUnitName">enum userunitname (user01, user02, user03)</param>
+        public UserUnit(string name, string symbol, double mass, UserUnitName userUnitName)
+        {
+            this.Name = name;
+            this.Symbol = symbol;
+            this.MassMonoIsotopic = mass;
+            this.UserUnitType = userUnitName;
         }
 
         /// <summary>
