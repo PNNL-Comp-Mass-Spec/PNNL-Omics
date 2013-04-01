bf4ca01b4e0c21711725c56dea458dd91245bc9b
Joseph Brown <joseph.brown@pnnl.gov>
2013-04-01 20:13:06 +0000

    Added Pearson correlation with p-value calculation for significance.

---
+++ PNNLOmics/Algorithms/Distance/PearsonCorrelation.cs
@@ -0,0 +1,184 @@
+﻿/* Written by Joseph N. Brown
+ * for the Department of Energy (PNNL, Richland, WA)
+ * Battelle Memorial Institute
+ * E-mail: joseph.brown@pnnl.gov
+ * Website: http://omics.pnl.gov/software
+ * -----------------------------------------------------
+ * 
+ * Notice: This computer software was prepared by Battelle Memorial Institute,
+ * hereinafter the Contractor, under Contract No. DE-AC05-76RL0 1830 with the
+ * Department of Energy (DOE).  All rights in the computer software are reserved
+ * by DOE on behalf of the United States Government and the Contractor as
+ * provided in the Contract.
+ * 
+ * NEITHER THE GOVERNMENT NOR THE CONTRACTOR MAKES ANY WARRANTY, EXPRESS OR
+ * IMPLIED, OR ASSUMES ANY LIABILITY FOR THE USE OF THIS SOFTWARE.
+ * 
+ * This notice including this sentence must appear on any copies of this computer
+ * software.
+ * -----------------------------------------------------*/
+
+using System;
+using System.Collections.Generic;
+
+namespace PNNLOmics.Algorithms.Distance
+{
+    /// <summary>
+    /// Class to calculate Pearson Product-Moment Correlation
+    /// </summary>
+    public class PearsonCorrelation
+    {
+        #region Properties
+        /// <summary>
+        /// Pearson correlation coefficient
+        /// </summary>
+        public double r { get; set; }
+
+        /// <summary>
+        /// Coefficient of Determination
+        /// </summary>
+        public double RSquared { get; set; }
+
+        /// <summary>
+        /// P-value of statistically significant correlation
+        /// </summary>
+        public double Pvalue { get; set; }
+
+        /// <summary>
+        /// Degrees of freedom for the study
+        /// </summary>
+        public int DegreesOfFreedom { get; set; }
+        #endregion
+        
+        #region Constructors
+        /// <summary>
+        /// General Constructor
+        /// </summary>
+        public PearsonCorrelation()
+        {
+        }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Calculates the Pearson Product-Moment Correlation Coefficient (r)
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns>True, if the function completes successfully</returns>
+        public bool Pearson(double[] x, double[] y)
+        {
+            if (x.Length != y.Length)
+                return false;
+
+            try
+            {
+                int n = x.Length;
+
+                r = alglib.pearsoncorr2(x, y, n);
+                RSquared = r * r;
+                DegreesOfFreedom = x.Length - 2;
+                                
+                Microsoft.Office.Interop.Excel.Application app = new Microsoft.Office.Interop.Excel.Application();
+                Microsoft.Office.Interop.Excel.WorksheetFunction wsf = app.WorksheetFunction;
+                Pvalue = wsf.TDist(
+                    (r * Math.Sqrt(DegreesOfFreedom) / Math.Sqrt(1 - r * r)),
+                    DegreesOfFreedom,
+                    2);
+                                
+                return true;
+            }
+            catch (Exception exc)
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Calculates the Pearson Product-Moment Correlation Coefficient (r)
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns>True, if the function completes successfully</returns>
+        public bool Pearson(List<double> x, List<double> y)
+        {
+            if (x.Count != y.Count)
+                return false;
+
+            return Pearson(x.ToArray(), y.ToArray());
+        }
+
+        /// <summary>
+        /// Calculates the Pearson Product-Moment Correlation Coefficient (r)
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns>True, if the function completes successfully</returns>
+        public bool Pearson(float[] x, float[] y)
+        {
+            if (x.Length != y.Length)
+                return false;
+
+            double[] x1 = new double[x.Length];
+            double[] y1 = new double[y.Length];
+            for (int i = 0; i < x.Length; i++)
+            {
+                x1[i] = x[i];
+                y1[i] = y[i];
+            }
+
+            return Pearson(x1, y1);
+        }
+
+        /// <summary>
+        /// Calculates the Pearson Product-Moment Correlation Coefficient (r)
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns>True, if the function completes successfully</returns>
+        public bool Pearson(List<float> x, List<float> y)
+        {
+            if (x.Count != y.Count)
+                return false;
+
+            return Pearson(x.ToArray(), y.ToArray());
+        }
+
+        /// <summary>
+        /// Calculates the Pearson Product-Moment Correlation Coefficient (r)
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns>True, if the function completes successfully</returns>
+        public bool Pearson(int[] x, int[] y)
+        {
+            if (x.Length != y.Length)
+                return false;
+
+            double[] x1 = new double[x.Length];
+            double[] y1 = new double[y.Length];
+            for (int i = 0; i < x.Length; i++)
+            {
+                x1[i] = x[i];
+                y1[i] = y[i];
+            }
+
+            return Pearson(x1, y1);
+        }
+
+        /// <summary>
+        /// Calculates the Pearson Product-Moment Correlation Coefficient (r)
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns>True, if the function completes successfully</returns>
+        public bool Pearson(List<int> x, List<int> y)
+        {
+            if (x.Count != y.Count)
+                return false;
+
+            return Pearson(x.ToArray(), y.ToArray());
+        }
+    }
+        #endregion
+}
