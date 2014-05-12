789687e4dcba7eed3e92152a6c117298bc8c3934
Scott Kronewitter <scott.kronewitter@pnnl.gov>
2014-05-12 23:50:05 +0000

    Added Unit Test placeholder for LC Alignment Tests

---
+++ PNNLOmics.UnitTests/AlgorithmTests/Alignment/LCMS_LCAlignment.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PNNLOmics.UnitTests.AlgorithmTests.Alignment
+{
+    class LCMS_LCAlignment
+    {
+    }
+}
