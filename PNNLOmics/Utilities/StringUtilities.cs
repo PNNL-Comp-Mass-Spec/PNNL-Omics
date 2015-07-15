b0e02ce37ffefbf5b1d831bda2f003afdd97c02c
alchemistmatt <matthew.monroe@pnnl.gov>
2015-07-15 12:45:06 -0700

    Add parameter limitDecimalsForLargeValues to DblToString

--- PNNLOmics/Utilities/StringUtilities.cs
+++ PNNLOmics/Utilities/StringUtilities.cs
@@ -14,41 +14,59 @@ namespace PNNLOmics.Utilities
         /// Format the value to a string with a fixed number of decimal points
         /// </summary>
         /// <param name="value">Value to format</param>
-        /// <param name="digitsOfPrecision">Digits to show after the decimal place (0 or higher)</param>
+        /// <param name="digitsAfterDecimal">Digits to show after the decimal place (0 or higher)</param>
+        /// <param name="limitDecimalsForLargeValues">When true, will limit the number of decimal points shown for values over 1</param>
         /// <returns>String representation of the value</returns>
         /// <remarks>If digitsOfPrecision is 0, will round the number to the nearest integer</remarks>
-        public static string DblToString(double value, byte digitsOfPrecision)
+        public static string DblToString(double value, byte digitsAfterDecimal, bool limitDecimalsForLargeValues = false)
         {
             if (Math.Abs(value) < float.Epsilon)
                 return "0";
 
-            if (digitsOfPrecision <= 0)
-                return value.ToString("0");
-
-            if (value >= 10 && Math.Abs(value - (int)value) < 1 / (Math.Pow(10, digitsOfPrecision)) / 2.0)
+            if (Math.Abs(value) >= 10 && Math.Abs(value - (int)value) < 1 / (Math.Pow(10, digitsAfterDecimal)) / 2.0)
             {
                 // Value 10 or larger and it is nearly an integer value (at least with respect to digitsOfPrecision)
                 // Return values like 10 or 150 instead of 10.000 or 150.000
                 return value.ToString("0");
             }
 
+            var effectiveDigitsAfterDecimal = digitsAfterDecimal;
+
+            if (Math.Abs(value) > 1 && limitDecimalsForLargeValues)
+            {
+                //  digitsLeftOfDecimal  = Math.Floor(Math.Log10(value)) + 1
+                //  digitsRightOfDecimal = digitsOfPrecision - (      digitsLeftOfDecimal         - 1)
+                //  digitsRightOfDecimal = digitsOfPrecision - (Math.Floor(Math.Log10(value)) + 1 - 1)
+
+                var digitsRightOfDecimal = digitsAfterDecimal - (byte)(Math.Floor(Math.Log10(value)));
+
+                if (digitsRightOfDecimal >= 0)
+                    effectiveDigitsAfterDecimal = (byte)digitsRightOfDecimal;
+                else
+                    effectiveDigitsAfterDecimal = 0;
+
+            }
+
+            if (effectiveDigitsAfterDecimal <= 0)
+                return value.ToString("0");
+
             string formatString;
-            if (mFormatStrings.TryGetValue(digitsOfPrecision, out formatString))
+            if (mFormatStrings.TryGetValue(effectiveDigitsAfterDecimal, out formatString))
             {
                 return value.ToString(formatString);
             }
 
             formatString = "0.0";
 
-            if (digitsOfPrecision > 1)
+            if (effectiveDigitsAfterDecimal > 1)
             {
                 // Update format string to be of the form "0.0#######"
-                formatString += new string('#', digitsOfPrecision - 1);
+                formatString += new string('#', effectiveDigitsAfterDecimal - 1);
             }
 
             try
             {
-                mFormatStrings.TryAdd(digitsOfPrecision, formatString);
+                mFormatStrings.TryAdd(effectiveDigitsAfterDecimal, formatString);
             }
             catch
             {
