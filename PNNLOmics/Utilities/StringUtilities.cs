5ae54789fb87ce7605afbac0de4fb0a5d02b1857
alchemistmatt <matthew.monroe@pnnl.gov>
2015-08-24 18:09:39 -0700

    Added DblToStringScientific

--- PNNLOmics/Utilities/StringUtilities.cs
+++ PNNLOmics/Utilities/StringUtilities.cs
@@ -12,17 +12,52 @@ namespace PNNLOmics.Utilities
         private static readonly ConcurrentDictionary<byte, string> mFormatStrings = new ConcurrentDictionary<byte, string>();
 
         /// <summary>
+        /// Dictionary that tracks the format string used for each digitsOfPrecision value displayed with scientific notation
+        /// </summary>
+        /// <remarks>
+        /// Keys are the number of digits of precision and
+        /// "false" if the format string is of the form 0.00E+00 or 
+        /// "true"  if the format string is of the form 0.00E+000
+        /// </remarks>
+        private static readonly ConcurrentDictionary<string, string> mFormatStringsScientific = new ConcurrentDictionary<string, string>();
+
+        /// <summary>
+        /// Format the value to a string with a fixed number of decimal points
+        /// </summary>
+        /// <param name="value">Value to format</param>
+        /// <param name="digitsAfterDecimal">Digits to show after the decimal place (0 or higher)</param>
+        /// <param name="limitDecimalsForLargeValues">When true, will limit the number of decimal points shown for values over 1</param>
+        /// <param name="thresholdScientific">Numbers below this level will be displayed using Scientific notation</param>
+        /// <returns>String representation of the value</returns>
+        /// <remarks>If digitsOfPrecision is 0, will round the number to the nearest integer</remarks>
+        public static string DblToString(
+            double value,
+            byte digitsAfterDecimal,
+            bool limitDecimalsForLargeValues = false,
+            double thresholdScientific = 0.001)
+        {
+            return DblToString(value, digitsAfterDecimal, limitDecimalsForLargeValues, true, thresholdScientific);
+        }
+
+        /// <summary>
         /// Format the value to a string with a fixed number of decimal points
         /// </summary>
         /// <param name="value">Value to format</param>
         /// <param name="digitsAfterDecimal">Digits to show after the decimal place (0 or higher)</param>
         /// <param name="limitDecimalsForLargeValues">When true, will limit the number of decimal points shown for values over 1</param>
         /// <param name="invariantCulture">When true (default) numbers will always use a period for the decimal point</param>
+        /// <param name="thresholdScientific">Numbers below this level will be displayed using Scientific notation</param>
         /// <returns>String representation of the value</returns>
         /// <remarks>If digitsOfPrecision is 0, will round the number to the nearest integer</remarks>
-        public static string DblToString(double value, byte digitsAfterDecimal, bool limitDecimalsForLargeValues = false, bool invariantCulture = true)
+        public static string DblToString(
+            double value,
+            byte digitsAfterDecimal,
+            bool limitDecimalsForLargeValues = false,
+            bool invariantCulture = true,
+            double thresholdScientific = 0.001)
         {
-            if (Math.Abs(value) < float.Epsilon)
+
+            if (Math.Abs(value) < double.Epsilon)
                 return "0";
 
             if (Math.Abs(value) >= 10 && Math.Abs(value - (int)value) < 1 / (Math.Pow(10, digitsAfterDecimal)) / 2.0)
@@ -30,6 +65,11 @@ namespace PNNLOmics.Utilities
                 // Value 10 or larger and it is nearly an integer value (at least with respect to digitsOfPrecision)
                 // Return values like 10 or 150 instead of 10.000 or 150.000
                 return value.ToString("0");
+            }
+
+            if (Math.Abs(value) < thresholdScientific)
+            {
+                return DblToStringScientific(value, digitsAfterDecimal, invariantCulture);
             }
 
             var effectiveDigitsAfterDecimal = digitsAfterDecimal;
@@ -69,6 +109,62 @@ namespace PNNLOmics.Utilities
             try
             {
                 mFormatStrings.TryAdd(effectiveDigitsAfterDecimal, formatString);
+            }
+            catch
+            {
+                // Ignore errors here
+            }
+
+            return value.ToString(formatString, invariantCulture ? NumberFormatInfo.InvariantInfo : NumberFormatInfo.CurrentInfo);
+        }
+
+        /// <summary>
+        /// Format the value to a string using scientific notation
+        /// </summary>
+        /// <param name="value">Value to format</param>
+        /// <param name="digitsAfterDecimal">Digits to show after the decimal place (0 or higher)</param>
+        /// <param name="invariantCulture">When true (default) numbers will always use a period for the decimal point</param>
+        /// <returns>String representation of the value</returns>
+        /// <remarks>If digitsOfPrecision is 0, will round the number to the nearest integer</remarks>
+        public static string DblToStringScientific(
+            double value,
+            byte digitsAfterDecimal,
+            bool invariantCulture = true)
+        {
+            if (Math.Abs(value) < double.Epsilon)
+                return "0";
+
+            if (digitsAfterDecimal <= 0)
+                return value.ToString("0");
+
+            var tinyNumber = Math.Log10(Math.Abs(value)) <= -99;
+
+            string formatString;
+            if (mFormatStringsScientific.TryGetValue(digitsAfterDecimal + tinyNumber.ToString(), out formatString))
+            {
+                return value.ToString(formatString, invariantCulture ? NumberFormatInfo.InvariantInfo : NumberFormatInfo.CurrentInfo);
+            }
+
+            formatString = "0.0";
+
+            if (digitsAfterDecimal > 1)
+            {
+                // Update format string to be of the form "0.0#######"
+                formatString += new string('#', digitsAfterDecimal - 1);
+            }
+
+            if (tinyNumber)
+            {
+                formatString += "E+000";
+            }
+            else
+            {
+                formatString += "E+00";
+            }
+
+            try
+            {
+                mFormatStringsScientific.TryAdd(digitsAfterDecimal + tinyNumber.ToString(), formatString);
             }
             catch
             {
